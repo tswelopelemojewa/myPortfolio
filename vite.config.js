93ea1f636fe0d56abcb6eff258c3223d715bea2d
tswelopelemojewa <tswelopelemojewa@gmail.com>
2023-07-03 09:48:55 +0200

    portfolio

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+
+import data from "./data.json" assert {type:'json'};
+
+import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite';
+// import data from './data.json';
+
+export default defineConfig({
+    base : '/responsive_webpages/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+                bootstrap: 'index-bootstrap.html',
+                materialize: 'index-materialize.html',
+                projects: 'projects.html'
+            }
+        },
+    },
+    plugins: [handlebars({
+      context : {
+        data
+      }
+  })]
+    
+});
